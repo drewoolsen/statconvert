a8e8db9c04f30b0d82ebcb89f5d11a0a4d23ad82
Nathan Hausspiegel <nhausspiegel@wesleyan.edu>
2023-12-04 18:29:44 -0500

    added functionality for chi-squared tests of independence

--- R/chisq_convert.R
+++ R/chisq_convert.R
@@ -1,4 +1,38 @@
-chisq_convert <- function(...) {
-  print("hello world!")
+# example from the paper:
+# A chi-square test of independence was performed to examine the relation
+# between religion and college interest. The relation between these variables
+# was significant, X2 (2, N = 170) = 14.14, p <.01
+# here I am assuming that "170" is the number of observations
+# i am not positive of that though
+# TODO: figure that out
+
+chisq_convert <- function(test_obj) {
+  if (test_obj$p.value < .001) {
+    test_obj$p.value.text <- "< .001"
+  } else if (test_obj$p.value < .005) {
+    test_obj$p.value.text <- "< .005"
+  } else if (test_obj$p.value < .01) {
+    test_obj$p.value.text <- "< .01"
+  } else if (test_obj$p.value < .05) {
+    test_obj$p.value.text <- "< .05"
+  } else {
+    test_obj$p.value.text <- paste("=", round(test_obj$p.value, 3))
+  }
+
+  test_obj$statistic.text <- round(test_obj$statistic, 3)
+  vars <- test_obj$data.name
+  signif <- ifelse(test_obj$p.value < .05, "significant", "not significant")
+
+  if (grepl("Pearson's Chi-squared test", test_obj$method[1], fixed=TRUE)) {
+    cat("A chi-square test of independence was performed.\n")
+    cat("The relation between ", vars[[1]][1], " was ", signif,
+        ", X² (df = ", test_obj$parameter, ") = ",
+        test_obj$statistic.text, ", p ", test_obj$p.value.text, ".\n", sep="")
+
+  } else if (test_obj$method[1] == "Chi-squared test for given probabilities") {
+    cat("A chi-square test of goodness-of-fit was performed.\n")
+    cat("[TODO: fill in this section]")
+  }
+
 }
 
--- R/statconvert.R
+++ R/statconvert.R
@@ -12,7 +12,7 @@ statconvert <- function(test_obj){
   ttest_method = c("Welch Two Sample t-test", "One Sample t-test", "Paired t-test", " Two Sample t-test")
   cortest_method = c("Pearson's product-moment correlation","Spearman's rank correlation rho",
                      "Kendall's rank correlation tau")
-  chisq_method = c("Pearson's Chi-squared test")
+  chisq_method = c("Pearson's Chi-squared test", "Chi-squared test for given probabilities")
 
   if(class(test_obj) == "htest" & test_obj$method[1] %in% ttest_method){
     ttest_convert(test_obj)
@@ -25,11 +25,13 @@ statconvert <- function(test_obj){
     oneway_anova_convert(test_obj)
   }
 
-  # as far as I can tell here are the two options for a chi-squared test method:
+  # as far as I can tell here are the two options for the method of
+  # a chi-squared test of independence:
   # "Pearson's Chi-squared test"
   # "Pearson's Chi-squared test with simulated p-value ..."
   # so we just check if the first string is a substring of method
-  if (grepl(chisq_method, test_obj$method[1], fixed=TRUE)) {
+  if (class(test_obj) == "htest" & test_obj$method[1] %in% chisq_method |
+      grepl("Pearson's Chi-squared test", test_obj$method[1], fixed=TRUE)) {
     chisq_convert(test_obj)
   }
 
@@ -45,10 +47,12 @@ statconvert(test1)
 statconvert(cor1)
 
 data("starwars")
-chi1 <- suppressWarnings(chisq.test(starwars$species, starwars$homeworld))
-chi2 <- suppressWarnings(chisq.test(starwars$gender, starwars$homeworld))
+chi1 <- chisq.test(starwars$species, starwars$homeworld)
+chi2 <- chisq.test(starwars$skin_color, starwars$hair_color)
+chi3 <- chisq.test(c(1,2,3), p = c(1/3, 1/3, 1/3))
 
 statconvert(chi1)
 statconvert(chi2)
+statconvert(chi3)
 
 
