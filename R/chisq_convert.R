5c028bc1aa51fbca6a0967453fe2d64dc7531969
Nathan Hausspiegel <nhausspiegel@wesleyan.edu>
2023-12-05 13:46:23 -0500

    added more details to chi-square test outputs

--- R/chisq_convert.R
+++ R/chisq_convert.R
@@ -5,6 +5,7 @@
 # here I am assuming that "170" is the number of observations
 # i am not positive of that though
 # TODO: should we include degrees of freedom and # of observations?
+# TODO: any more info to include?
 
 chisq_convert <- function(test_obj) {
   if (test_obj$p.value < .001) {
@@ -21,26 +22,29 @@ chisq_convert <- function(test_obj) {
 
   test_obj$statistic.text <- round(test_obj$statistic, 3)
   vars <- test_obj$data.name
-  signif <- ifelse(test_obj$p.value < .05, "significant", "not significant")
+  signif <- test_obj$p.value < .05
+  data_string <- paste0("X² (df = ", test_obj$parameter, ") = ",
+        test_obj$statistic.text, ", p ", test_obj$p.value.text)
 
   if (grepl("Pearson's Chi-squared test", test_obj$method[1], fixed=TRUE)) {
+    is_significant <- ifelse(signif, "", "not ")
     cat("A chi-square test of independence was performed.\n")
-    cat("The relation between ", vars[[1]][1], " was ", signif,
-        ", X² (df = ", test_obj$parameter, ") = ",
-        test_obj$statistic.text, ", p ", test_obj$p.value.text, ".\n", sep="")
+    cat("The relation between ", vars[[1]][1], " is ", is_significant,
+        "significant, ", data_string, ".\n", sep="")
 
   } else if (test_obj$method[1] == "Chi-squared test for given probabilities") {
+    is_distributed <- ifelse(signif, "not ", "")
 
     # is the distribution uniform?
-    if (var(test_obj$expected) == 0) {
-      is_uniform = "uniformly"
-    } else is_uniform = "according to a custom distribution"
+    if (var(test_obj$expected) == 0) is_uniform = "uniformly"
+    else is_uniform = "according to the distribution provided"
 
-    cat("A chi-square test of goodness-of-fit was performed to determine if the elements of ",
-        vars, " were distributed ", is_uniform, ".\n", sep="")
-    # TODO:
-    cat("[TODO: fill in this section]\n")
+    cat("A chi-square test of goodness-of-fit was performed.\n")
+    cat("The elements of ", vars, " are ", is_distributed, "distributed ",
+        is_uniform, ", ", data_string, ".\n", sep="")
   }
+
+  else stop()
 
 }
 
